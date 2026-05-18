5b78bc6bb155abfe13ff82d345a9966849bb2156
gjandiriz <82677087+gjandiriz@users.noreply.github.com>
2026-05-18 12:15:17 -0300

    Update estaciones.js

--- js/estaciones.js
+++ js/estaciones.js
@@ -1,1 +1,23 @@
+console.log("js_estaciones cargado OK");
 
+function guardarEstacion() {
+    const nombre = document.getElementById("est-nombre").value.trim();
+    const descripcion = document.getElementById("est-desc").value.trim();
+
+    if (!nombre) {
+        alert("Ingresá el nombre de la estación.");
+        return;
+    }
+
+    callServer("crearNueva_estacion", {
+        nombre: nombre,
+        descripcion: descripcion
+    }, res => {
+        alert(res);
+
+        document.getElementById("est-nombre").value = "";
+        document.getElementById("est-desc").value = "";
+
+        mostrar("pantalla-menu");
+    });
+}
